f635dbf25802c0c86ba8faa092edb2ee6ae18932
Angus Edwards <angus.s.edwards@gmail.com>
2022-03-25 14:41:53 +0000

    test broken

--- SERVER/controller/event-controller.js
+++ SERVER/controller/event-controller.js
@@ -3,7 +3,6 @@ const eventModel = require('../models/event-model')
 const postEvents = async (req, res) => {
   try {
     const event = req.body
-    const test = 'string'
     const savedEvent = await eventModel.create(event)
   } catch (error) {
     console.log(error)
